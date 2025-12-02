feebfafcfd35872fc7ca235dac97f5452f5d6023
dAppCaP <cf_github@appcap.nl>
2025-12-02 15:37:21 +0100

    fix: Show Statisctics only on Homepage
    Note that `/masterblocks` (currently?) acts as the homepage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { ConfigService } from './config.service';
 import { SearchBarComponent } from './searchbar/searchbar.component';
 import { StatisticsComponent } from './statistics/statistics.component';
@@ -7,13 +9,33 @@ import { StatisticsComponent } from './statistics/statistics.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, SearchBarComponent, StatisticsComponent],
+  imports: [RouterModule, NgIf, SearchBarComponent, StatisticsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'INTERCHAIN Explorer';
+  showStatistics = false;
 
-  // The constructor of ConfigService will run, which validates the configuration
-  constructor(private config: ConfigService) { }
+  constructor(
+    private config: ConfigService,
+    private router: Router
+  ) {
+    // Initial state on first load
+    this.showStatistics = this.isHomeUrl(this.router.url);
+
+    // React to navigation changes
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(e => {
+        this.showStatistics = this.isHomeUrl(e.urlAfterRedirects);
+      });
+  }
+
+  // Determine if the given URL is the homepage
+  // '/masterblocks' (currently?) acts as the homepage
+  private isHomeUrl(url: string): boolean {
+    const clean = url.split('?')[0];
+    return clean === '/masterblocks' || clean === '/masterblocks/';
+  }
 }
