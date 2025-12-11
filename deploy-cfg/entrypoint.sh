9a75e9056debfc7584ac87b28dbd657b8aa74d22
ShadowsCrypto <shadows@crypto-factor.io>
2025-12-11 12:19:46 +0200

    fix: environment for prod deployments

--- deploy-cfg/entrypoint.sh
+++ deploy-cfg/entrypoint.sh
@@ -2,14 +2,12 @@
 
 [[ -z "${APP_BASE_URL}" ]] && apiBaseUrl="http://localhost" || apiBaseUrl="${APP_BASE_URL}"
 [[ -z "${APP_PROD_FREQ}" ]] && apiProdFreq="60000" || apiProdFreq="${APP_PROD_FREQ}"
-[[ -z "${EXTERNAL_EXPLORER_DMC}" ]] && defichainExplorerBaseUrl="" || defichainExplorerBaseUrl="${EXTERNAL_EXPLORER_DMC}"
-[[ -z "${EXTERNAL_EXPLORER_PBC}" ]] && partisiaExplorerBaseUrl="" || partisiaExplorerBaseUrl="${EXTERNAL_EXPLORER_PBC}"
-[[ -z "${EXTERNAL_EXPLORER_POL}" ]] && polygonExplorerBaseUrl="" || polygonExplorerBaseUrl="${EXTERNAL_EXPLORER_POL}"
+[[ -z "${EXTERNAL_EXPLORERS_JSON}" ]] && externalExplorers="" || externalExplorers="${EXTERNAL_EXPLORERS_JSON}"
+[[ -z "${STATE_VALIDATION_CHAIN_ID}" ]] && stateValidationChainId="" || stateValidationChainId="${STATE_VALIDATION_CHAIN_ID}"
 
 find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{apiBaseUrl}}|$apiBaseUrl|g"
 find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{apiProdFreq}}|$apiProdFreq|g"
-find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{defichainExplorerBaseUrl}}|$defichainExplorerBaseUrl|g"
-find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{partisiaExplorerBaseUrl}}|$partisiaExplorerBaseUrl|g"
-find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{polygonExplorerBaseUrl}}|$polygonExplorerBaseUrl|g"
+find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{externalExplorers}}|$externalExplorers|g"
+find /usr/share/nginx/html/ -type f -name "main*.js" -print0 | xargs -0 sed -i "s|{{stateValidationChainId}}|$stateValidationChainId|g"
 
 nginx -g "daemon off;"
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,6 @@ export const environment = {
   APP_BASE_URL: '{{apiBaseUrl}}',
   APP_POLL_FREQ: '{{apiProdFreq}}',
 
-  externalExplorers: {
-    1131: { name: 'DeFiMetaChain', baseUrl: '{{defichainExplorerBaseUrl}}', prefixWith0x: true },
-    18500: { name: 'PartisiaBlockchain', baseUrl: '{{partisiaExplorerBaseUrl}}', prefixWith0x: false },
-    80002: { name: 'PolygonScan', baseUrl: '{{polygonExplorerBaseUrl}}', prefixWith0x: true },
-  } as Record<number, { name: string; baseUrl: string; prefixWith0x?: boolean }>,
+  externalExplorers: JSON.parse("{{externalExplorers}}"),
+  stateValidationChainId: Number.parseInt("{{stateValidationChainId}}"),
 };
